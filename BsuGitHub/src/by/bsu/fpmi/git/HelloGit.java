57a11813217f33ab726c1337e21bbd08bdd507e4
fpm.mazelyuk <fpm.mazelyuk@bsu.by>
2012-03-02 20:04:21 +0500

    1
    Signed-off-by: fpm.mazelyuk <fpm.mazelyuk@bsu.by>

---
+++ BsuGitHub/src/by/bsu/fpmi/git/HelloGit.java
@@ -0,0 +1,13 @@
+package by.bsu.fpmi.git;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
